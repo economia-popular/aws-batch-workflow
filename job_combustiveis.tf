64fb90c2f71125a23e6715003c2c6d79203ccd2c
Matheus Fidelis <matheus@nanoshots.com.br>
2023-02-05 22:52:26 -0300

    Job Index

--- job_combustiveis.tf
+++ job_combustiveis.tf
@@ -4,7 +4,7 @@ resource "aws_batch_job_definition" "indices_combustiveis" {
   type = "container"
 
   timeout {
-    attempt_duration_seconds = var.job_timeout * 2
+    attempt_duration_seconds = var.job_timeout * 3
   }
 
   platform_capabilities = [
